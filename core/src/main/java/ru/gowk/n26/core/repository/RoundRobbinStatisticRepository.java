8062a6a64efcbfee68f37f9a1a9d1ae4bfa80018
Vyacheslav Gorbatykh <vyacheslav@gorbatih.ru>
2017-12-14 02:08:33 +0100

    create documentation

--- core/src/main/java/ru/gowk/n26/core/repository/RoundRobbinStatisticRepository.java
+++ core/src/main/java/ru/gowk/n26/core/repository/RoundRobbinStatisticRepository.java
@@ -9,6 +9,23 @@ import ru.gowk.n26.domain.Transaction;
 import java.util.stream.IntStream;
 
 /**
+ * Domain and index.
+ * To drop old transactions from statistics we use domain and index. See {@link #getDomain(long)} and
+ * {@link #getIndex(long)}.
+ * When we add new transaction, we store it in appropriate item and set it's domain.
+ * Then, when we receive statistics, we use domain to understand - is this item contains statistic for last
+ * statisticPeriod.
+ * <p>
+ * For example, statisticPeriod = 60000 and current time is 1513210240500. Index would be 500 and domain - 25220170
+ * <p>
+ * All items in {@link #statistics} from index 0 to index 500 (including) are milliseconds of this minute within
+ * current time window. So the domain value of items should be the same as the domain value of current time. If not -
+ * it's an old statistic and we should skip it.
+ * <p>
+ * And all items in {@link #statistics} from 500 (excluding) to the end of array are milliseconds of previous minute
+ * within current time window. So the domain value of items should be the same as the (domain value of current time -
+ * 1). If not - it's an old statistic and we should skip it.
+ *
  * @author Vyacheslav Gorbatykh
  * @since 11.12.2017
  */
@@ -52,10 +69,17 @@ public class RoundRobbinStatisticRepository implements StatisticRepository {
     }
 //-----------------------------------------------------------------------------
 
+    /**
+     * index - it's a number of statistic's item where we should store the transaction with this timestamp.
+     */
     private int getIndex(long timestamp) {
         return (int) (timestamp % statistics.length);
     }
 
+    /**
+     * domain - it's a serial number of "time window" for which corresponds the transaction with this timestamp.
+     * For example, if statisticPeriod=1min, then this is the serial number of minute in epoch.
+     */
     private long getDomain(long timestamp) {
         return timestamp / statistics.length;
     }
@@ -71,7 +95,8 @@ public class RoundRobbinStatisticRepository implements StatisticRepository {
         }
     }
 
-    private void accumulate(Statistic result, long currentDomain, int currentIndex, PreliminaryStatistic source, int sourceIndex) {
+    private void accumulate(Statistic result, long currentDomain, int currentIndex, PreliminaryStatistic source,
+                            int sourceIndex) {
         //noinspection SynchronizationOnLocalVariableOrMethodParameter
         synchronized (source) {
             if (sourceIndex <= currentIndex) {
